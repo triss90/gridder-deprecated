2a201e15f544c321226abac6f1ef9070c6d6406d
triss90405 <oliver.tristan@gmail.com>
2017-05-15 18:07:22 +0200

    Add installation guide

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  $gridderFileName = "gridder-1.4.1.css";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +16,6 @@
 <body>
 
   <!--Content-->
-
   <header class="header">
     <div class="container">
 
@@ -118,10 +121,29 @@
         <div class="medium-12">
           <div class="label">Getting started</div>
           <h2>Quick installation guide</h2>
+          <p>Simply <a href="/gridder/compiled/<?php echo $gridderFileName; ?>" download>download</a> gridder and add it to your head, like this:</p>
+          <pre><code class="html codeblock">
+// index.html
+&#x3C;html&#x3E;
+  &#x3C;head&#x3E;
+    &#x3C;meta name=&#x22;viewport&#x22; content=&#x22;width=device-width, initial-scale=1&#x22;&#x3E;
+    &#x3C;link href=&#x22;path/to/gridder.css&#x22; rel=&#x22;stylesheet&#x22;&#x3E;
+  &#x3C;/head&#x3E;
+  &#x3C;body&#x3E;
+    &#x3C;div id=&#x22;app&#x22;&#x3E;&#x3C;/div&#x3E;
+  &#x3C;/body&#x3E;
+&#x3C;/html&#x3E;
+          </code></pre>
+          <p>Remember to add <span class="label inline dark">&#x3C;meta name=&#x22;viewport&#x22; content=&#x22;width=device-width, initial-scale=1&#x22;&#x3E;</span> to the head aswell, to make sure the site scales correctly</p>
+          <br><p>Gridder is also available through CDNJS:</p>
+          <pre><code class="html codeblock">
+https://cdnjs.cloudflare.com/ajax/libs/3Dmol/1.1.0/3Dmol-min.js
+          </code></pre>
         </div>
       </div>
     </div>
   </section>
+
 
   <section id="faq">
     <div class="container">
