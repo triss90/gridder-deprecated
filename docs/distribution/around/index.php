af28b7ff0277614cd4251fdc7ab119738f0f42fc
triss90405 <oliver.tristan@gmail.com>
2017-05-21 11:00:15 +0200

    Add distribution documentation

---
+++ docs/distribution/around/index.php
@@ -0,0 +1,67 @@
+<?php
+  $root = $_SERVER['SERVER_NAME'];
+  $gridderFileName = "gridder-1.4.1.css";
+  $gridderFilePath = '//'.$root."/gridder/compiled/gridder-1.4.1.css";
+  $pageTitle = "Gridder - Alignment - Middle";
+  $pageDescription = "With Gridder you can force columns to the middle of a row.";
+?>
+
+<?php include_once('../../../inc/header.php') ?>
+
+<div class="documentation_wrapper">
+
+  <div class="documentation_inner">
+
+    <?php include_once('../../../inc/doc_nav.php') ?>
+
+    <div class="documentation_content">
+
+      <div class="row">
+        <div class="tiny">
+          <div class="breadcrumbs">
+            <a href="/docs" class="crumb">Documentation</a>
+            <a href="/docs/distribution" class="crumb">Distribution</a>
+            <a href="#0" class="crumb">Around</a>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="tiny">
+          <h1>Alignment (Around)</h1>
+          <p><span class="label inline dark">around-*</span> will equally destribute space around your columns.</p>
+
+          <pre><code class="html codeblock">
+&#x3C;div class=&#x22;row around-tiny&#x22;&#x3E;
+  &#x3C;div class=&#x22;tiny-3&#x22;&#x3E;
+    Space Around
+  &#x3C;/div&#x3E;
+  &#x3C;div class=&#x22;tiny-3&#x22;&#x3E;
+    Space Around
+  &#x3C;/div&#x3E;
+&#x3C;/div&#x3E;
+          </code></pre>
+
+          <h2>Example</h2>
+          <div class="row around-tiny example-row">
+            <div class="tiny-3">
+              <div class="example-box">Space Around</div>
+            </div>
+            <div class="tiny-3">
+              <div class="example-box">Space Around</div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php include_once('../../../inc/footer.php') ?>
---
+++ docs/distribution/index.php
@@ -0,0 +1,46 @@
+<?php
+  $root = $_SERVER['SERVER_NAME'];
+  $gridderFileName = "gridder-1.4.1.css";
+  $gridderFilePath = '//'.$root."/gridder/compiled/gridder-1.4.1.css";
+?>
+
+<?php include_once('../../inc/header.php') ?>
+
+<div class="documentation_wrapper">
+
+  <div class="documentation_inner">
+
+    <?php include_once('../../inc/doc_nav.php') ?>
+
+    <div class="documentation_content">
+
+      <div class="row">
+        <div class="tiny">
+          <div class="breadcrumbs">
+            <a href="/docs" class="crumb">Documentation</a>
+            <a href="#0" class="crumb">Distribution</a>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="tiny">
+          <h1>Distribution</h1>
+          <p>Add one or more of the following classes to distribute the content of a row or column, according to the flexbox properties <span class="label inline dark">justify-content: space-around;</span> or <span class="label inline dark">justify-content: space-between;</span></p>
+          <ul>
+            <li><a href="/docs/distribution/between/">Between</a></li>
+            <li><a href="/docs/distribution/around/">Around</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php include_once('../../inc/footer.php') ?>
