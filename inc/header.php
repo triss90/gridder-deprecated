068c57368cb78899d7c7f13519e5b92dca5ced43
triss90405 <oliver.tristan@gmail.com>
2017-05-22 14:18:47 +0200

    Add favicon

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,24 @@
   <meta name="author" content="Tristan White">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="//<?php echo $root; ?>/assets/css/style.css">
-  <link rel="icon" type="image/ico" href="//<?php echo $root; ?>/assets/img/favicon.ico">
+  <link rel="apple-touch-icon" sizes="57x57" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="//<?php echo $root; ?>/assets/img/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="//<?php echo $root; ?>/assets/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="//<?php echo $root; ?>/assets/img/android-chrome-192x192.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="//<?php echo $root; ?>/assets/img/favicon-16x16.png">
+  <link rel="manifest" href="//<?php echo $root; ?>/assets/img/manifest.json">
+  <link rel="mask-icon" href="//<?php echo $root; ?>/assets/img/safari-pinned-tab.svg" color="#333">
+  <link rel="shortcut icon" href="//<?php echo $root; ?>/assets/img/favicon.ico">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="//<?php echo $root; ?>/assets/img/mstile-144x144.png">
+  <meta name="msapplication-config" content="//<?php echo $root; ?>/assets/img/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
 </head>
 <body>
