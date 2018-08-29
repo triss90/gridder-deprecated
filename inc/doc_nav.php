bef242327e1f8c5ceb5a231b6a31f8714fa6f4ba
Tristan White <oliver.tristan@gmail.com>
2018-08-29 08:36:59 +0200

    Update logo on documentation pages

--- inc/doc_nav.php
+++ inc/doc_nav.php
@@ -5,14 +5,18 @@
 
 <nav class="documentation_nav">
   <a href="/" class="logo">
-    <svg id="gridder-logo" width="40px" height="40px" viewBox="0 0 315 313" xmlns="http://www.w3.org/2000/svg">
-      <title>Gridder Logo</title>
-      <desc>Gridder Logo</desc>
-      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <path d="M306.328883,98.5887695 C311.117809,101.337614 315,108.035396 315,113.573366 L315,257.559076 C315,271.370015 305.297934,276.997004 293.318607,270.120866 L126.671117,174.465149 C121.882191,171.716304 118,165.018522 118,159.480552 L118,15.4948421 C118,1.68390288 127.702066,-3.94308625 139.681393,2.93305188 L306.328883,98.5887695 Z" id="rect-2" fill="#f8f8f8"></path>
-        <path d="M188.328883,137.588769 C193.117809,140.337614 197,147.035396 197,152.573366 L197,296.559076 C197,310.370015 187.297934,315.997004 175.318607,309.120866 L8.67111664,213.465149 C3.88219116,210.716304 0,204.018522 0,198.480552 L0,54.4948421 C0,40.6839029 9.70206587,35.0569138 21.6813933,41.9330519 L188.328883,137.588769 Z" id="rect-1" fill="#f8f8f8"></path>
-      </g>
-    </svg>
+      <svg id="gridder-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 201">
+          <circle class="a" cx="25" cy="25" r="25"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(150)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(75 75)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(150 150)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(1 150)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(75)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(0 75)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(150 75)"/>
+          <circle class="a" cx="25" cy="25" r="25" transform="translate(75 151)"/>
+          <path class="b" d="M150,0H0V151H150V75H75" transform="translate(25 25)"/>
+      </svg>
     <span class="text">Gridder</span>
   </a>
 
